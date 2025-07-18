b9bfee7c9825005debb51886bbb53ee0d08c7967
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-07-18 18:44:08 +0530

    Create Solution.cpp

---
+++ A. Only One Digit/Solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+void solve(ll t){
+    ll x;
+    cin>>x;
+    ll ans=11;
+    while(x){
+        ll digit=x%10;
+        x=x/10;
+        ans=min(ans,digit);
+    }
+    cout<<ans<<'\n';
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i);
+  }
+  return 0;
+}
