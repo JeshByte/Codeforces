d99548d8d68051ed5b466614b360f62ae8972d1a
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-12-12 15:23:36 +0530

    Create Solution.cpp

---
+++ A. Operations with Inversions/Solution.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void solve(ll t){
+    ll n;
+    cin>>n;
+    vector<ll> arr;
+    for(int i=0;i<n;i++){
+        ll input;
+        cin>>input;
+        arr.push_back(input);
+    }
+    ll maxi=arr[0];
+    ll ans=0;
+    for(int i=1;i<n;i++){
+        if(maxi>arr[i]){
+            ans++;
+        }
+        else{
+            maxi=arr[i];
+        }
+    }
+    cout<<ans<<'\n';
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i);
+  }
+  return 0;
+}
