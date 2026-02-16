9d7d1bc86bc6e3caf53ae602e29890890e3f34c0
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-02-16 23:47:02 +0530

    Create Solution.cpp

---
+++ A. Sieve of Erato67henes/Solution.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void solve(ll t){
+    ll n;
+    cin>>n;
+    vector<ll> arr;
+    bool poss=false;
+    for(int i=0;i<n;i++){
+        ll input;
+        cin>>input;
+        if(input==67){
+            poss=true;
+        }
+        arr.push_back(input);
+    }
+    if(poss){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i);
+  }
+  return 0;
+}
