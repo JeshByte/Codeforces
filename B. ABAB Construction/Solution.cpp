aeb700e3e84c8be5fdeb264bc609e7ceef1ef2f1
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-02-26 01:19:16 +0530

    Create Solution.cpp

---
+++ B. ABAB Construction/Solution.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void solve(ll t){
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    if(n%2==0){
+        for(int i=0;i<n;i+=2){
+            if(s[i]==s[i+1] && s[i]!='?'){
+                cout<<"NO\n";
+                return;
+            }
+        }
+        cout<<"YES\n";
+    }
+    else{
+        if(s[0]=='b'){
+            cout<<"NO\n";
+            return;
+        }
+        for(int i=1;i<n;i+=2){
+            if(s[i]==s[i+1] && s[i]!='?'){
+                cout<<"NO\n";
+                return;
+            }
+        }
+        cout<<"YES\n";
+    }
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i);
+  }
+  return 0;
+}
