1a3fe8bcb2d1c25ddda0a9a19d8df053b2c7b24b
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-03-17 23:31:51 +0530

    Create Solution.cpp

---
+++ A. Bingo Candies/Solution.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void solve(ll t,ll testcases){
+    ll n;
+    cin>>n;
+    vector<vector<ll>> arr(n,vector<ll>(n));
+    map<ll,ll> mp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+            mp[arr[i][j]]++;
+        }
+    }
+    if(n==1){
+        cout<<"NO\n";
+        return;
+    }
+    ll cnt=(n*n)-n;
+    for(auto it:mp){
+        if(it.second>cnt){
+            cout<<"NO\n";
+            return;
+        }
+    }
+    cout<<"YES\n";
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i,t);
+  }
+  return 0;
+}
