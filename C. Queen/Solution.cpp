ec75d5e26248fc9c9173ddd51fc7602273d097cb
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-03-25 12:20:06 +0530

    Create Solution.cpp

---
+++ C. Queen/Solution.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void printWithPrecision(auto n){
+    cout << fixed << setprecision(6) << n << '\n';
+}
+class DSU{
+    vector<ll>parent,rank;
+    public:
+    DSU(ll n){
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    ll findParent(ll v){
+        if(v==parent[v]){
+            return v;
+        }
+        return parent[v]=findParent(parent[v]);
+    }
+    void Union(ll u,ll v){
+        ll pu=findParent(u);
+        ll pv=findParent(v);
+        if(pu==pv){
+            return;
+        }
+        if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }
+        else if(rank[pv]<rank[pu]){
+            parent[pv]=pu;
+        }
+        else{
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+};
+bool dfs(vector<ll>& ans,vector<vector<ll>>& child,map<ll,ll>& respect,ll v){
+    //cout<<v<<'\n';
+    bool valid=true;
+    for(auto it:child[v]){
+        bool check=dfs(ans,child,respect,it);
+        valid=valid&&check;
+    }
+    if(valid && respect[v]==1){
+        ans.push_back(v);
+    }
+    if(respect[v] == 1){
+        //cout<<v<<'\n';
+        return true;
+    }
+    return false;
+}
+void solve(ll t,ll testcases){
+    ll n;
+    cin>>n;
+    vector<vector<ll>> child(n+1,vector<ll>());
+    map<ll,ll> respect;
+    ll root=-1;
+    for(int i=1;i<=n;i++){
+        ll p,r;
+        cin>>p>>r;
+        respect[i]=r;
+        if(p==-1){
+            root=i;
+            continue;
+        }
+        child[p].push_back(i);
+    }
+    // for(auto it:respect){
+    //     cout<<it.first<<" "<<it.second<<'\n';
+    // }
+    // for(int i=1;i<=n;i++){
+    //     cout<<i<<":";
+    //     for(auto it:child[i]){
+    //         cout<<it<<" ";
+    //     }
+    //     cout<<'\n';
+    // }
+    vector<ll> ans;
+    dfs(ans,child,respect,root);
+    sort(ans.begin(),ans.end());
+    if(ans.empty()){
+        cout<<-1<<'\n';
+        return;
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<'\n';
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  //cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i,t);
+  }
+  return 0;
+}
