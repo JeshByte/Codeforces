8a227f90455c7e0d2e6fcd80509f4a71215ac703
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-04-01 22:22:02 +0530

    Create Solution.cpp

---
+++ C1. A Simple GCD Problem (Easy Version)/Solution.cpp
@@ -0,0 +1,74 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void printWithPrecision(auto n){
+    cout << fixed << setprecision(6) << n << '\n';
+}
+void solve(ll t,ll testcases){
+    ll n;
+    cin>>n;
+    vector<ll> a,b;
+    for(int i=0;i<n;i++){
+        ll input;
+        cin>>input;
+        a.push_back(input);
+    }
+    for(int i=0;i<n;i++){
+        ll input;
+        cin>>input;
+        b.push_back(input);
+    }
+    ll cnt=0;
+    for(int i=0;i<=n-1;i++){
+        ll g;
+        if(i==0){
+            g=__gcd(a[i],a[i+1]);
+        }
+        else if(i==n-1){
+            g=__gcd(a[i],a[i-1]);
+        }
+        else{
+            ll g1=__gcd(a[i],a[i+1]);
+            ll g2=__gcd(a[i],a[i-1]);
+            g=(g1*g2)/(__gcd(g1,g2));
+        }
+        if(a[i]>g){
+            cnt++;
+        }
+    }
+    //ll ans=n-res.size();
+    cout<<cnt<<'\n';
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i,t);
+  }
+  return 0;
+}
