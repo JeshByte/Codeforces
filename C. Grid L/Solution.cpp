d25dc16c0f58bc865416c08d754c2e1748ca5087
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-04-16 09:53:30 +0530

    Create Solution.cpp

---
+++ C. Grid L/Solution.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void printWithPrecision(auto n){
+    cout << fixed << setprecision(6) << n << '\n';
+}
+void solve(ll t,ll testcases){
+    ll p,q;
+    cin>>p>>q;
+    for(ll n=1;n<=1e5;n++){
+        ll term1=p+2*q-n;
+        ll term2=2*n+1;
+        ll m=term1/term2;
+        if(term1%term2==0 && m>=n){
+            ll qmax=n*(n+1)+n*(m-n);
+            if(q<=qmax){
+                cout<<n<<" "<<m<<'\n';
+                return;
+            }
+        }
+    }
+    cout<<-1<<'\n';
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i,t);
+  }
+  return 0;
+}
+// for interactive
+// cout<<" "<<endl;
+// fflush(stdout);
