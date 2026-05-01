5fc51d8e676e89aaad17dae8385ba9e209d351b6
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2026-05-01 09:31:56 +0530

    Create Solution.cpp

---
+++ A. Disturbing Distribution/Solution.cpp
@@ -0,0 +1,75 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+ll modExpo(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
+ll divideModulo(ll a, ll b, ll m) {
+    ll b_inv = findInverse(b, m);
+    return ((a % m) * b_inv) % m;
+}
+void printWithPrecision(auto n){
+    cout << fixed << setprecision(6) << n << '\n';
+}
+ll MOD=676767677;
+void solve(ll t,ll testcases){
+    ll n;
+    cin>>n;
+    vector<ll> arr;
+    for(int i=0;i<n;i++){
+        ll input;
+        cin>>input;
+        arr.push_back(input);
+    }
+    ll ind=-1;
+    for(int i=n-1;i>=0;i--){
+        if(arr[i]!=1){
+            ind=i;
+            break;
+        }
+    }
+    if(ind==-1){
+        cout<<1<<'\n';
+    }
+    else{
+        ll ans=0;
+        for(int i=0;i<=ind;i++){
+            if(arr[i]!=1){
+                ans=(ans+arr[i])%MOD;
+            }
+        }
+        if(ind<(n-1)){
+            ans=(ans+1)%MOD;
+        }
+        cout<<ans<<'\n';
+    }
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i,t);
+  }
+  return 0;
+}
+// for interactive
+// cout<<" "<<endl;
+// fflush(stdout);
